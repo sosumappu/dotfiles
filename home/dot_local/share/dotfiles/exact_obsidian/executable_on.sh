dfab479974ab805f3b0598f6cce11d56d8109716
Adel Arab <adelarab.works@gmail.com>
2024-11-01 15:01:59 +0100

    fix obsidian executable on

--- home/dot_local/share/dotfiles/exact_obsidian/executable_on.sh
+++ home/dot_local/share/dotfiles/exact_obsidian/executable_on.sh
@@ -4,9 +4,10 @@ if [ -z "$1" ]; then
   echo "Error: a file name must be set, e.g. \"Butterflies Algorithmic\""
   exit 1
 fi
-
-file_name=$(echo "$1" | tr ' ' '-' | tr '[A-Z]' '[a-z]')
+file_name="${1// /-}"           # Replace spaces with hyphens
+file_name="${file_name:l}"      # Convert to lowercase
 formatted_file_name=$(date "+%Y-%m-%d")_"${file_name}".md
 cd "$OBSIDIAN_VAULT" || exit 2
 touch "inbox/${formatted_file_name}"
-lvim -c "normal! <leader>oo" "inbox/${formatted_file_name}"
+lvim -c "ObsidianTemplate note" -c "execute 'normal! dd'" -c "execute 'normal! 20G'" -c "s/# \d\{4}-\d\{2}-\d\{2}_/\# /g" -c "s/-/ /g" -c "nohlsearch" "inbox/${formatted_file_name}"
+
