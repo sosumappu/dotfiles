cc3d6d0c2d139a07ded9d9e12d5992edfb2465bc
Adel Arab <adelarab.works@gmail.com>
2024-11-03 14:48:34 +0100

    fix obsidian 'fleeting' dir in aliases & scripts

--- home/dot_local/share/dotfiles/exact_obsidian/executable_on.sh
+++ home/dot_local/share/dotfiles/exact_obsidian/executable_on.sh
@@ -8,6 +8,6 @@ file_name="${1// /-}"           # Replace spaces with hyphens
 file_name="${file_name:l}"      # Convert to lowercase
 formatted_file_name=$(date "+%Y-%m-%d")_"${file_name}".md
 cd "$OBSIDIAN_VAULT" || exit 2
-touch "inbox/${formatted_file_name}"
-lvim -c "ObsidianTemplate note" -c "execute 'normal! dd'" -c "execute 'normal! 20G'" -c "s/# \d\{4}-\d\{2}-\d\{2}_/\# /g" -c "s/-/ /g" -c "nohlsearch" "inbox/${formatted_file_name}"
+touch "fleeting/${formatted_file_name}"
+lvim -c "ObsidianTemplate note" -c "execute 'normal! dd'" -c "execute 'normal! 20G'" -c "s/# \d\{4}-\d\{2}-\d\{2}_/\# /g" -c "s/-/ /g" -c "nohlsearch" "fleeting/${formatted_file_name}"
 
